a17b6c234353fb6fc6b1fe87d2095db9329a4b76
Ugur Timurcin <utimurcin@outlook.com>
2023-01-11 20:00:56 +0300

    changed homepage to loginview

--- lib/main.dart
+++ lib/main.dart
@@ -11,19 +11,19 @@ void main() {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomePage(),
+      home: const LoginView(),
     ),
   );
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+class LoginView extends StatefulWidget {
+  const LoginView({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<LoginView> createState() => _LoginViewState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _LoginViewState extends State<LoginView> {
   late final TextEditingController _email;
   late final TextEditingController _password;
 
