5afc4c92f040a7b06045493523392c5c8c5652b0
Ugur Timurcin <utimurcin@outlook.com>
2023-01-16 13:08:03 +0300

    extracted show log out dialog and moved to separate file

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -2,9 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/consts/routes.dart';
 import 'package:mynotes/enums/menu_action.dart';
-import 'dart:developer' as devtools show log;
-
-import 'package:mynotes/views/login_view.dart';
+import '../utilities/show_log_out_dialog.dart';
 
 class NotesView extends StatefulWidget {
   const NotesView({super.key});
@@ -49,30 +47,4 @@ class _NotesViewState extends State<NotesView> {
       body: const Text('Hello World'),
     );
   }
-}
-
-Future<bool> showLogOutDialog(BuildContext context) {
-  return showDialog<bool>(
-    context: context,
-    builder: (context) {
-      return AlertDialog(
-        title: const Text('Sign out'),
-        content: const Text('Are you sure want to sign out?'),
-        actions: [
-          TextButton(
-            onPressed: () {
-              Navigator.of(context).pop(false);
-            },
-            child: const Text('Cancel'),
-          ),
-          TextButton(
-            onPressed: () {
-              Navigator.of(context).pop(true);
-            },
-            child: const Text('Log out'),
-          ),
-        ],
-      );
-    },
-  ).then((value) => value ?? false);
 }
