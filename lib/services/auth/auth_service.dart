b1380420c064fb6543acd38ecc2c4beb29867768
Ugur Timurcin <utimurcin@outlook.com>
2023-01-16 23:39:24 +0300

    added forgotten initialize method

--- lib/services/auth/auth_service.dart
+++ lib/services/auth/auth_service.dart
@@ -1,10 +1,16 @@
 import 'package:mynotes/services/auth/auth_provider.dart';
 import 'package:mynotes/services/auth/auth_user.dart';
+import 'package:mynotes/services/auth/firebase_auth_provider.dart';
 
 class AuthService implements AuthProvider {
   final AuthProvider _provider;
 
   const AuthService(this._provider);
+
+  factory AuthService.firebase() => AuthService(FirebaseAuthProvider());
+
+  @override
+  Future<void> initialize() => _provider.initialize();
 
   @override
   Future<AuthUser> createUser({
