350230c76fd71955d705cd2b78c67c7fc342931b
Ugur Timurcin <utimurcin@outlook.com>
2023-01-18 23:31:23 +0300

    renamed file and class name

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:mynotes/consts/routes.dart';
 import 'package:mynotes/services/auth/auth_service.dart';
 import 'package:mynotes/views/login_view.dart';
-import 'package:mynotes/views/notes/new_note_view.dart';
+import 'package:mynotes/views/notes/create_update_note_view.dart';
 import 'package:mynotes/views/notes/notes_view.dart';
 import 'package:mynotes/views/register_view.dart';
 import 'package:mynotes/views/verify_email_view.dart';
@@ -35,7 +35,7 @@ void main() {
                 type: PageTransitionType.leftToRight);
           case newNoteRoute:
             return PageTransition(
-                child: const NewNoteView(),
+                child: const CreateUpdateNoteView(),
                 type: PageTransitionType.bottomToTop);
           default:
             return null;
