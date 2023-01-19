5a5e3a78a7b459443c6a540433c51e2be4feca10
Ugur Timurcin <utimurcin@outlook.com>
2023-01-20 00:02:10 +0300

    get cloud note from constructor

--- lib/services/cloud/firebase_cloud_storage.dart
+++ lib/services/cloud/firebase_cloud_storage.dart
@@ -18,11 +18,18 @@ class FirebaseCloudStorage {
 
   factory FirebaseCloudStorage() => _shared;
 
-  void createNewNote({required String ownerUserId}) async {
-    await notes.add({
+  Future<CloudNote> createNewNote({required String ownerUserId}) async {
+    final document = await notes.add({
       ownerUserIdFieldName: ownerUserId,
       textFieldName: '',
     });
+    final fetchedNote = await document.get();
+
+    return CloudNote(
+      documentId: fetchedNote.id,
+      ownerUserId: ownerUserId,
+      text: '',
+    );
   }
 
   Future<void> updateNote({
@@ -58,11 +65,7 @@ class FirebaseCloudStorage {
           .get()
           .then(
             (value) => value.docs.map(
-              (doc) => CloudNote(
-                documentId: doc.id,
-                ownerUserId: doc.data()[ownerUserIdFieldName] as String,
-                text: doc.data()[textFieldName] as String,
-              ),
+              (doc) => CloudNote.fromSnapshot(doc),
             ),
           );
     } catch (_) {
