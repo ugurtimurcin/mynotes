d2e5edb61a0aead55b4f48395e16d856373a4a14
Ugur Timurcin <utimurcin@outlook.com>
2023-01-19 17:14:32 +0300

    implemented crud operations for cloud storage

---
+++ lib/services/cloud/firebase_cloud_storage.dart
@@ -0,0 +1,72 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:mynotes/services/cloud/cloud_note.dart';
+import 'package:mynotes/services/cloud/cloud_storage_constants.dart';
+import 'package:mynotes/services/cloud/cloud_storage_exceptions.dart';
+
+class FirebaseCloudStorage {
+  final notes = FirebaseFirestore.instance.collection('notes');
+  Stream<Iterable<CloudNote>> allNotes({required String ownerUserId}) =>
+      notes.snapshots().map(
+            (event) => event.docs
+                .map((doc) => CloudNote.fromSnapshot(doc))
+                .where((note) => note.ownerUserId == ownerUserId),
+          );
+
+  static final FirebaseCloudStorage _shared =
+      FirebaseCloudStorage._sharedInstance();
+  FirebaseCloudStorage._sharedInstance();
+
+  factory FirebaseCloudStorage() => _shared;
+
+  void createNewNote({required String ownerUserId}) async {
+    await notes.add({
+      ownerUserIdFieldName: ownerUserId,
+      textFieldName: '',
+    });
+  }
+
+  Future<void> updateNote({
+    required String documentId,
+    required String text,
+  }) async {
+    try {
+      await notes.doc(documentId).update(
+        {
+          textFieldName: text,
+        },
+      );
+    } catch (_) {
+      throw CouldNotUpdateNoteExcepiton();
+    }
+  }
+
+  Future<void> deleteNote({required String documentId}) async {
+    try {
+      await notes.doc(documentId).delete();
+    } catch (_) {
+      throw CouldNotDeleteNoteExcepiton();
+    }
+  }
+
+  Future<Iterable<CloudNote>> getNotes({required String ownerUserId}) async {
+    try {
+      return await notes
+          .where(
+            ownerUserIdFieldName,
+            isEqualTo: ownerUserId,
+          )
+          .get()
+          .then(
+            (value) => value.docs.map(
+              (doc) => CloudNote(
+                documentId: doc.id,
+                ownerUserId: doc.data()[ownerUserIdFieldName] as String,
+                text: doc.data()[textFieldName] as String,
+              ),
+            ),
+          );
+    } catch (_) {
+      throw CouldNotGetAllNotesExcepiton();
+    }
+  }
+}
